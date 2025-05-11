ddffc388fe0c17914a0d6d6432b381598e2412cb
Atomic Potato <55362397+Atomic-Potato@users.noreply.github.com>
2025-05-11 16:12:05 +0300

    Add files via upload

---
+++ RestoreDefaultCPUState.ps1
@@ -0,0 +1,21 @@
+﻿# This script is used to reset your Power Plan max state when youre done with CPU State Manager
+
+
+################ PARAMETERS ################
+
+$default_cpu_max_state = 99
+$power_plan_name = 'MyPowerSaver'
+
+################ PARAMETERS ################
+
+
+$guid = powercfg /list | ForEach-Object {
+    if ($_ -match "Power Scheme GUID: ([a-f0-9\-]+).*?\($power_plan_name\)") {
+        return $matches[1]
+    }
+}
+
+
+powercfg -setacvalueindex $guid SUB_PROCESSOR PROCTHROTTLEMAX $default_cpu_max_state
+powercfg -setdcvalueindex $guid SUB_PROCESSOR PROCTHROTTLEMAX $default_cpu_max_state
+powercfg -setactive $guid
